a38318b388819a6fa7c597dafbc80d456785bbf2
Alif_Rahman <alifjcc99@gmail.com>
2021-05-21 15:32:29 +0600

    Add files via upload

---
+++ basic_data_types_hackerrank.cpp
@@ -0,0 +1,19 @@
+
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int integer;
+    long longInteger;
+    char character;
+    float floating;
+    double doubleValu;
+    
+    scanf("%d %ld %c %f %lf", &integer,&longInteger, &character, &floating, &doubleValu);
+    printf("%d\n%ld\n%c\n%f\n%lf",integer,longInteger, character, floating, doubleValu);
+    return 0;
+}
+
