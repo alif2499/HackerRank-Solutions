2336d4642bd414225fc39d06832611527af435ee
Alif_Rahman <alifjcc99@gmail.com>
2021-05-24 23:07:13 +0600

    Add files via upload

---
+++ cats_and_a_mouse_hackerrank.py
@@ -0,0 +1,11 @@
+
+
+# Complete the catAndMouse function below.
+def catAndMouse(x, y, z):
+    if abs(x-z) < abs(y-z):
+        return "Cat A"
+    elif abs(x-z) > abs(y-z):
+        return "Cat B"
+    else:
+        return "Mouse C"
+
