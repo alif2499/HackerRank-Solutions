f55e3b73bc902c06871f0fd466b9b8d988be7703
Alif_Rahman <alifjcc99@gmail.com>
2022-04-22 12:05:25 +0600

    Update Counting Sort_hackerrank.py

--- Counting Sort_hackerrank.py
+++ Counting Sort_hackerrank.py
@@ -6,12 +6,12 @@ import random
 import re
 import sys
 
-#
+
 # Complete the 'countingSort' function below.
 #
 # The function is expected to return an INTEGER_ARRAY.
 # The function accepts INTEGER_ARRAY arr as parameter.
-#
+
 
 def countingSort(arr):
     # Write your code here
