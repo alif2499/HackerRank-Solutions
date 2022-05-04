1b503475a39b7872eaf168f3e2106afd6c48ecad
Alif_Rahman <alifjcc99@gmail.com>
2022-05-04 23:23:06 +0600

    Update Lonely Integer_hackerrank.py

--- Lonely Integer_hackerrank.py
+++ Lonely Integer_hackerrank.py
@@ -6,12 +6,12 @@ import random
 import re
 import sys
 
-#
+
 # Complete the 'lonelyinteger' function below.
 #
 # The function is expected to return an INTEGER.
 # The function accepts INTEGER_ARRAY a as parameter.
-#
+
 
 def lonelyinteger(a):
     # Write your code here
